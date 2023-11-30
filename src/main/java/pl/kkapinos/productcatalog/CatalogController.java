d7450a55137c5fe237c64f5777d78828914667bd
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2023-11-30 17:57:45 +0100

    new endpoint in CatalogController

--- src/main/java/pl/kkapinos/productcatalog/CatalogController.java
+++ src/main/java/pl/kkapinos/productcatalog/CatalogController.java
@@ -1,6 +1,10 @@
 package pl.kkapinos.productcatalog;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +21,17 @@ public class CatalogController {
     @GetMapping("/api/products")
     List<Product> allProducts() {
         return productCatalog.allPublishedProducts();
+    }
+
+    @PostMapping("/api/products/create")
+    public ResponseEntity<String> addProduct(@RequestParam String name, @RequestParam String desc) {
+        try {
+            String productId = productCatalog.addProduct(name, desc);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Product added successfully. Product ID: " + productId);
+        } catch (ProductCantBePublishedException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Product cannot be published.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to add product.");
+        }
     }
 }
